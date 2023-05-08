63b53a7f206982a481067126ec051584ded293fd
tastyfish <crazychris32@gmail.com>
2023-05-08 19:02:21 -0400

    vue3, loading wheel support

--- src/App.vue
+++ src/App.vue
@@ -2,54 +2,57 @@
   <div id="app">
     <OrderTree :nodes="nodes" :scrollOrderCounter="scrollOrderCounter" @select="select" />
     <TileGrid :strip="root" class="grid" />
+    <div id="app-loading" v-if="isLoading">
+      <ProgressSpinner />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-/* eslint class-methods-use-this: [
-  "error", { "exceptMethods": ["root", "nodes", "scrollOrderCounter", "select"
-] }] */
 
-import {
-  Component, Vue,
-} from 'vue-property-decorator';
-import { StripProvider } from '@/api/strip';
-import { generateLineTree, TreeNode } from '@/api/tree';
+import { defineComponent } from 'vue';
+import { generateLineTree } from '@/api/tree';
 import TileGrid from '@/components/TileGrid.vue';
 import OrderTree from '@/OrderTree.vue';
 import generateTiles from '@/tiles';
 import { choiceSlots } from '@/menu';
 
-import vxm, { ChoiceMenuMode } from '@/store';
+import { ChoiceMenuMode, useOrderStore, useUIStore } from '@/store';
+import { mapStores } from 'pinia';
 
-@Component({
+export default defineComponent({
   components: {
     TileGrid,
     OrderTree,
   },
+  computed: {
+    root() {
+      return generateTiles();
+    },
+    nodes() {
+      return generateLineTree(
+        this.orderStore.lines,
+        this.orderStore.choices,
+        choiceSlots,
+        this.orderStore.currentLineID,
+      );
+    },
+    scrollOrderCounter() {
+      return this.orderStore.scrollOrderCounter
+    },
+    isLoading() {
+      return this.uiStore.isLoading
+    },
+    ...mapStores(useOrderStore, useUIStore),
+  },
+  methods: {
+    select(lineID: number) {
+      this.orderStore.setCurrentLine(lineID)
+      this.uiStore.setChoiceMenuMode(ChoiceMenuMode.Default)
+    }
+  }
 })
-export default class App extends Vue {
-  get root(): StripProvider {
-    return generateTiles();
-  }
 
-  get nodes(): TreeNode[] {
-    return generateLineTree(
-      vxm.order.lines, vxm.order.choices,
-      choiceSlots, vxm.order.currentLineID,
-    );
-  }
-
-  get scrollOrderCounter(): number {
-    return vxm.order.scrollOrderCounter;
-  }
-
-  // An order was selected.
-  select(lineID: number): void {
-    vxm.order.setCurrentLine(lineID);
-    vxm.ui.setChoiceMenuMode(ChoiceMenuMode.Default);
-  }
-}
 </script>
 
 <style lang="scss">
@@ -65,6 +68,15 @@ body {
   display: flex;
   flex-direction: row;
   align-items: stretch;
+}
+#app-loading {
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+  background-color: #000C;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
 
