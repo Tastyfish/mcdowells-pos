7779af9c763fc5001b788b07ee95a99c086a8cc4
tastyfish <crazychris32@gmail.com>
2023-05-08 19:02:37 -0400

    vue3

--- src/OrderTree.vue
+++ src/OrderTree.vue
@@ -4,53 +4,53 @@
 </template>
 
 <script lang="ts">
-/* eslint class-methods-use-this: ["error", { "exceptMethods": ["selectedKeys"] }] */
 
-import {
-  Component, Vue, Ref, Watch, Prop,
-} from 'vue-property-decorator';
-import { TreeNode } from '@/api/tree';
+import { defineComponent, PropType, ComponentPublicInstance } from 'vue'
+import Tree, { TreeExpandedKeys, TreeNode } from 'primevue/tree';
 
-interface NodeExpansionInfo {
-  [key: number]: boolean
-}
+export default defineComponent({
+  props: {
+    nodes: {
+      type: Array as PropType<TreeNode[]>,
+      required: true as true,
+    },
+    scrollOrderCounter: Number,
+  },
+  emits: ['select'],
+  computed: {
+    treeComponent() { return this.$refs.tree as Tree },
+    selectedKeys() { return [] as (string | number)[] },
+    expandedNodes(): TreeExpandedKeys {
+      return this.$props.nodes.reduce((dict, node) => ({ ...dict, [node.key as string]: true }), {})
+    },
+  },
+  methods: {
+    // On node selection
+    select(node: TreeNode) {
+      // Number before : in a node key is always the order line ID.
+      this.$emit('select', parseInt((node.key as string).split(':')[0], 10))
+    }
+  },
+  watch: {
+    scrollOrderCounter() {
+      // Scroll to bottom after rendering.
+      this.$nextTick(() => {
+        const treeInstance = this.treeComponent as Partial<ComponentPublicInstance>
 
-@Component({})
-export default class App extends Vue {
-  @Ref('tree') readonly treeComponent!: Vue
-
-  @Prop(Array) readonly nodes!: TreeNode[]
-
-  @Prop(Number) readonly scrollOrderCounter!: number
-
-  get expandedNodes(): NodeExpansionInfo {
-    return this.nodes.reduce((dict, node) => ({ ...dict, [node.key]: true }), {});
+        treeInstance.$el.scrollTop = treeInstance.$el.scrollHeight
+      });
+    }
   }
+})
 
-  @Watch('scrollOrderCounter')
-  onNodesChanged(): void {
-    // Scroll to bottom after rendering.
-    this.$nextTick(() => {
-      this.treeComponent.$el.scrollTop = this.treeComponent.$el.scrollHeight;
-    });
-  }
-
-  get selectedKeys(): (string | number)[] {
-    return [];
-  }
-
-  // On node selection
-  select(node: TreeNode): void {
-    // Number before : in a node key is always the order line ID.
-    this.$emit('select', parseInt((node.key as string).split(':')[0], 10));
-  }
-}
 </script>
 
 <style scoped>
+
 .tree {
   width: 33.33%;
   margin: 0.5em;
   overflow-y: auto;
 }
+
 </style>
