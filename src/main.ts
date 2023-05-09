588dfe06f35c6982e4b4622bd946adebbdeac004
tastyfish <crazychris32@gmail.com>
2023-05-08 23:13:09 -0400

    add primeflex, up to date theme

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,10 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import { setupPrime } from './prime';
 
-import 'primevue/resources/themes/arya-blue/theme.css';
+import 'primevue/resources/themes/lara-dark-blue/theme.css';
 import 'primevue/resources/primevue.min.css';
 import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
 
 const app = createApp(App)
     .use(createPinia())
