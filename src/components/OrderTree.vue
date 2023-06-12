ba48688b869dc531e85ad36ef7e75da71bf9a2e7
tastyfish <crazychris32@gmail.com>
2023-06-12 13:26:10 -0400

    prettier vue files

--- src/components/OrderTree.vue
+++ src/components/OrderTree.vue
@@ -1,64 +1,67 @@
 <script setup lang="ts">
-
 import { ComponentPublicInstance, computed, watch, nextTick, ref } from 'vue'
-import Tree, { TreeExpandedKeys, TreeNode } from 'primevue/tree';
-import { currency } from '@/config/locale.json';
+import Tree, { TreeExpandedKeys, TreeNode } from 'primevue/tree'
+import { currency } from '@/config/locale.json'
 
 const props = defineProps<{
-  nodes: TreeNode[]
-  scrollOrderCounter: number
-}>();
+    nodes: TreeNode[]
+    scrollOrderCounter: number
+}>()
 
 const emits = defineEmits<{
-  select: [lineID: number]
-}>();
+    select: [lineID: number]
+}>()
 
-const treeComponent = ref(null as Tree | null);
+const treeComponent = ref(null as Tree | null)
 
-const selectedKeys = computed(() => [] as (string | number)[]);
+const selectedKeys = computed(() => [] as (string | number)[])
 
 // Expand every branch always.
 const expandedNodes = computed((): TreeExpandedKeys => props.nodes.reduce((dict, node) => ({ ...dict, [node.key as string]: true }), {}))
 
 // On node selection
 function select(node: TreeNode) {
-  // Number before : in a node key is always the order line ID.
-  emits('select', parseInt((node.key as string).split(':')[0], 10))
+    // Number before : in a node key is always the order line ID.
+    emits('select', parseInt((node.key as string).split(':')[0], 10))
 }
 
 watch(
-  () => props.scrollOrderCounter,
-  async () => {
-    // Scroll to bottom after rendering.
-    await nextTick();
+    () => props.scrollOrderCounter,
+    async () => {
+        // Scroll to bottom after rendering.
+        await nextTick()
 
-    const treeInstance = treeComponent.value as Partial<ComponentPublicInstance>
+        const treeInstance = treeComponent.value as Partial<ComponentPublicInstance>
 
-    treeInstance.$el.scrollTop = treeInstance.$el.scrollHeight
-  }
-);
-
+        treeInstance.$el.scrollTop = treeInstance.$el.scrollHeight
+    }
+)
 </script>
 
 <template>
-  <Tree class="order-tree" ref="treeComponent" :value="nodes" :expandedKeys="expandedNodes"
-  selectionMode="single" :selectionKeys="selectedKeys" @node-select="select">
-    <template #default="slotProps">
-      {{ slotProps.node.label }}
-    </template>
-    <template #priced="slotProps">
-      <div class="w-full flex flex-row">
-        <span class="flex-auto">{{ slotProps.node.label }}</span>
-        <b>{{ currency }}{{ (slotProps.node.data as number).toFixed(2) }}</b>
-      </div>
-    </template>
-  </Tree>
+    <Tree
+        class="order-tree"
+        ref="treeComponent"
+        :value="nodes"
+        :expandedKeys="expandedNodes"
+        selectionMode="single"
+        :selectionKeys="selectedKeys"
+        @node-select="select"
+    >
+        <template #default="slotProps">
+            {{ slotProps.node.label }}
+        </template>
+        <template #priced="slotProps">
+            <div class="w-full flex flex-row">
+                <span class="flex-auto">{{ slotProps.node.label }}</span>
+                <b>{{ currency }}{{ (slotProps.node.data as number).toFixed(2) }}</b>
+            </div>
+        </template>
+    </Tree>
 </template>
 
 <style scoped>
-
 .order-tree:deep(.p-treenode-label) {
-  width: 100%;
+    width: 100%;
 }
-
 </style>
--- src/components/TileGrid.vue
+++ src/components/TileGrid.vue
@@ -1,55 +1,54 @@
-<script setup lang='ts'>
-
+<script setup lang="ts">
 import { ProvidedStrip, emptyStrip } from '@/api/strip'
 import TileView from './TileView.vue'
 
-const DIM_X = 10;
-const DIM_Y = 10;
+const DIM_X = 10
+const DIM_Y = 10
 
-withDefaults(defineProps<{
-  strip: ProvidedStrip
-}>(), {
-  strip: () => emptyStrip(DIM_X, DIM_Y),
-})
-
+withDefaults(
+    defineProps<{
+        strip: ProvidedStrip
+    }>(),
+    {
+        strip: () => emptyStrip(DIM_X, DIM_Y),
+    }
+)
 </script>
 
 <template>
-  <div class="tile-grid">
-    <div class="tile-row" v-for="y in DIM_X" :key="y">
-      <div class="tile-cell" v-for="x in DIM_Y" :key="x">
-        <TileView :tile="strip.getTile(x - 1, y - 1, DIM_X, DIM_Y)" />
-      </div>
+    <div class="tile-grid">
+        <div class="tile-row" v-for="y in DIM_X" :key="y">
+            <div class="tile-cell" v-for="x in DIM_Y" :key="x">
+                <TileView :tile="strip.getTile(x - 1, y - 1, DIM_X, DIM_Y)" />
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
-<style scoped lang='scss'>
-
+<style scoped lang="scss">
 .tile-grid {
-  display: flex;
-  flex-direction: column;
+    display: flex;
+    flex-direction: column;
 }
 
 .tile-row {
-  min-height: 10%;
-  max-height: 10%;
-  display: flex;
-  align-items: stretch;
-  padding: 0.25em 0;
+    min-height: 10%;
+    max-height: 10%;
+    display: flex;
+    align-items: stretch;
+    padding: 0.25em 0;
 }
 
 .tile-cell {
-  min-width: 10%;
-  max-width: 10%;
-  padding: 0em 0.25em;
+    min-width: 10%;
+    max-width: 10%;
+    padding: 0em 0.25em;
 
-  * {
-    min-width: 100%;
-    max-width: 100%;
-    min-height: 100%;
-    max-height: 100%;
-  }
+    * {
+        min-width: 100%;
+        max-width: 100%;
+        min-height: 100%;
+        max-height: 100%;
+    }
 }
-
 </style>
