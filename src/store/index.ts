32131e867d6c00456d8f51deff61a76f8f6cd815
tastyfish <crazychris32@gmail.com>
2023-06-12 10:59:26 -0400

    First class export SmartOrderPayload

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,6 @@
-export { useOrderStore } from './order';
-export { useUIStore } from './ui';
+export { useOrderStore } from './order'
+export { useUIStore } from './ui'
 
-export { NO_CURRENT_LINE } from './order';
-export type { OrderCount } from './order';
-export { ChoiceMenuMode, TileScreen } from './ui';
+export { NO_CURRENT_LINE } from './order'
+export type { OrderCount, SmartOrderPayload } from './order'
+export { ChoiceMenuMode, TileScreen } from './ui'
