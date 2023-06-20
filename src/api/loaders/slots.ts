076877c98a0b4b4b4383ad6942330e2ac810a127
tastyfish <crazychris32@gmail.com>
2023-06-19 20:21:26 -0400

    Cleanup of slot loader

--- src/api/loaders/slots.ts
+++ src/api/loaders/slots.ts
@@ -4,17 +4,12 @@ import { ChoiceSlot, isValidChoiceSlot } from '../menu'
 
 type RawSlotSchema = Record<string, Omit<ChoiceSlot, 'id'>>
 
-function sanitizeSlot(slot: ChoiceSlot): ChoiceSlot | null {
-    return isValidChoiceSlot(slot) ? slot : null
-}
-
 function parseSlots(rawSlots: RawSlotSchema): Record<string, ChoiceSlot> {
     return Object.fromEntries(
-        (
-            Object.entries(rawSlots)
-                .map(([id, slot]) => sanitizeSlot({ id, ...slot }))
-                .filter((slot) => slot !== null) as ChoiceSlot[]
-        ).map((slot) => [slot.id, slot])
+        Object.entries(rawSlots)
+            .map(([id, slot]) => ({ id, ...slot }))
+            .filter((slot) => isValidChoiceSlot(slot))
+            .map((slot) => [slot.id, slot])
     )
 }
 
